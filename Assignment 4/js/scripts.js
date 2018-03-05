a42e350b7ff155699405d6c268a09bec80977c78
Unknown <vangr4@gmatc.matc.edu>
2018-03-05 13:05:48 -0600

    Assignment 4 finished w/JS

--- Assignment 4/js/scripts.js
+++ Assignment 4/js/scripts.js
@@ -2,4 +2,11 @@ var greeting = 'HEY! ';
 var name = 'Dude';
 var message = ',please check your order:';
 
-var welcome = greeting + name + message; 
+var welcome = greeting + name + message;
+document.getElementsById('greeting').textContent="welcome";
+
+var price = 20,
+var studentDiscount = .10,
+var studentPrice = price - (price * studentDiscount);
+document.getElementsById('price').textContent="newtext";
+document.getElementsById('student-price').textContent="newtext";
